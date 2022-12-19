3f4fd1c5641fd7d75c3892cdaa9a9afee5844cb5
yisj777s <111944054+yisj777s@users.noreply.github.com>
2022-12-19 12:05:27 +0900

    Update object.js

--- 자바스크립트 기초강의/7. 오브젝트 넌 뭐니/javascript/object.js
+++ 자바스크립트 기초강의/7. 오브젝트 넌 뭐니/javascript/object.js
@@ -90,7 +90,8 @@ const user4 = Object.assign({}, user);
 console.log(user4);
 
 //another example
-
-
-
-
+const fruit1 = { color: 'red'};
+const fruit2 = { color: 'blue', size: 'big'};
+const mixed = Object.assign({}, fruit1, fruit2)
+console.log(mixed.color);
+console.log(mixed.size);
