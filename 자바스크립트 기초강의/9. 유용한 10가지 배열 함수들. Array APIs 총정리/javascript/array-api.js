ff869c49e7a0d59a7d96f4201ab7f8e93adebb3f
yisj777s <111944054+yisj777s@users.noreply.github.com>
2022-12-26 12:07:04 +0900

    Update array-api.js

--- 자바스크립트 기초강의/9. 유용한 10가지 배열 함수들. Array APIs 총정리/javascript/array-api.js
+++ 자바스크립트 기초강의/9. 유용한 10가지 배열 함수들. Array APIs 총정리/javascript/array-api.js
@@ -45,31 +45,59 @@ class Student {
   
   // Q5. find a student with the score 90
   {
-  }
+    const result = students.find((student) => student.score === 90);
+    // const result = students.find(function(student){
+    //   return student.score === 90;
+    // });  
+    console.log(result);
+    }
   
   // Q6. make an array of enrolled students
   {
+    const result = students.filter((student) => student.enrolled);
+    console.log(result);
   }
   
   // Q7. make an array containing only the students' scores
   // result should be: [45, 80, 90, 66, 88]
   {
+    const result = students.map((student)=>student.score);
+    // const result = students.map(function(student){
+    //   return student.score;
+    // });
+    console.log(result);
   }
   
   // Q8. check if there is a student with the score lower than 50
   {
+    console.clear();
+    const result = students.some((student) => student.score < 50);
+    console.log(result);
+
+    const result2 = !students.every((student)=> student.score >= 50);
+    console.log(result2)
+    console.log(!true)
   }
   
   // Q9. compute students' average score
   {
+    console.clear();
+    const result = students.reduce((prev, curr) => prev + curr.score,0);
+    console.log(result/students.length);
   }
   
   // Q10. make a string containing all the scores
   // result should be: '45, 80, 90, 66, 88'
   {
+    const result = students.map((student) => student.score).filter((score)=>score >= 50).join();
+    console.log(result);
   }
   
   // Bonus! do Q10 sorted in ascending order
   // result should be: '45, 66, 80, 88, 90'
   {
+    const result = students.map(student => student.score).sort((a,b)=>a-b).join();
+    const result2 = students.map(student => student.score).sort((a,b)=>b-a).join();
+    console.log(result);
+    console.log(result2);
   }
