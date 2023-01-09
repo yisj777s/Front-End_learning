f4792fd006160deba1ece9808fc5f1423fa2f675
yisj777s <111944054+yisj777s@users.noreply.github.com>
2023-01-09 18:02:45 +0900

    Update callback.js

--- 자바스크립트 기초강의/11. 비동기 처리의 시작 콜백 이해하기, 콜백 지옥 체험 😱 JavaScript Callback/javascript/async/callback.js
+++ 자바스크립트 기초강의/11. 비동기 처리의 시작 콜백 이해하기, 콜백 지옥 체험 😱 JavaScript Callback/javascript/async/callback.js
@@ -18,3 +18,51 @@ function printWithDelay(print, timeout){
     setTimeout(print,timeout);
 }
 printWithDelay(()=>console.log('async callback'),2000);
+
+//Callback Hell example
+class UserStorage {
+    loginUser(id, password, onSuccess, onError){
+        setTimeout(() => {
+            if (
+                (id === 'ellie' && password ==='dream') ||
+                (id === 'coder' && password ==='academy' )
+            ) {
+                onSuccess(id);
+            } else {
+                onError(new Error('not found'));
+            }
+        }, 2000);
+        }
+
+    getRoles(user, onSuccess, onError){
+        setTimeout(()=>{
+            if (user === 'ellie'){
+                onSuccess({ name: 'ellie', role: 'admin'});
+            } else{
+                onError(new Error('no access'));
+            }
+        },1000);
+        
+    }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(
+    id, 
+    password, 
+    user => {
+        userStorage.getRoles(
+            user, 
+            userWithRole => {
+            alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
+        },
+        error => {
+            console.log(error);
+        })
+    },
+    error => {
+        console.log(error);
+    }
+);
