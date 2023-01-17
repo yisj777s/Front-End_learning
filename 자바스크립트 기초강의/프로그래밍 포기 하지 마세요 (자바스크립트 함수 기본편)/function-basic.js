fc12c6d3ef646978e67042a241ed38ed8950caa6
yisj777s <111944054+yisj777s@users.noreply.github.com>
2023-01-17 12:12:45 +0900

    Add files via upload

---
+++ 자바스크립트 기초강의/프로그래밍 포기 하지 마세요 (자바스크립트 함수 기본편)/function-basic.js
@@ -0,0 +1,27 @@
+// Don't give up
+// 포기하지 마세요 🙌
+
+//함수 선언
+function doSomething(add) {
+    console.log(add);
+    const result = add(2,3);
+    console.log(result);
+}
+
+function add(a, b){
+    const sum = a + b;
+    return sum;
+}
+
+// 함수 호출
+// doSomething(add);
+
+const addFun = add;
+console.log(add);
+addFun(1, 2);
+
+const result =  addFun(4,2);
+
+console.log(result);
+
+
